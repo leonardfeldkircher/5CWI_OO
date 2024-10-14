02f158dee57486958f17ca87d1dc389f0183287f
leonard <you@example.com>
2024-10-14 14:31:22 +0200

    working..

---
+++ src/Ticket/Printer.java
@@ -0,0 +1,11 @@
+package Ticketautomat;
+
+public class Printer {
+    
+    public Ticket PrintTicket()
+    {
+        Ticket ticket = new Ticket();
+
+        return ticket;
+    }
+}
--- src/Ticket/Ticket.java
+++ src/Ticket/Ticket.java
@@ -1,7 +1,35 @@
+package Ticketautomat;
+
+import java.util.Random;
+
+
 public class Ticket {
+    private int id;
+    private long createdTimeStamp;
+    private long payedTimeStamp;
+    private Random random = new Random();
 
-private id
-private tsCreated
-private 
+    public Ticket()
+    {
+        createdTimeStamp = System.currentTimeMillis();
+        id = random.nextInt();
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public long getCreatedTimeStamp() {
+        return createdTimeStamp;
+    }
+
+    public long getPayedTimeStamp() {
+        return payedTimeStamp;
+    }
+
+    public void setPayedTimeStamp(long payedTimeStamp) {
+        this.payedTimeStamp = payedTimeStamp;
+    }
+
+    
 }
